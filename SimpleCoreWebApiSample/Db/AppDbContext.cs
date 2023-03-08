8447ce1f7893b4f919cd98a3089dea736e7c37a8
Charles Path <charles.path@sinqia.com.br>
2023-03-08 02:05:09 -0300

    First Commit after starting Project

---
+++ SimpleCoreWebApiSample/Db/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleCoreWebApiSample.Entities;
+
+namespace SimpleCoreWebApiSample.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
